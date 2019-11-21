59ec0c18ca817d89f85d3fd816fe8eaf854e1aa6
Andreas Wikström <andreas.wikstrom@scania.com>
2019-11-21 10:33:57 +0100

    Simplified importing of corporate-ui - Changed the way we import corporate-ui

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { applyPolyfills, defineCustomElements, addTheme } from 'corporate-ui-dev';
+import { defineCustomElements, addTheme } from 'corporate-ui-dev';
 import { theme as scania } from 'scania-theme';
 
 import { AppModule } from './app/app.module';
@@ -14,7 +14,5 @@ if (environment.production) {
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
-applyPolyfills().then(() => {
-  defineCustomElements(window);
-  addTheme(scania);
-});
+defineCustomElements(['c-header', 'c-content']);
+addTheme(scania);
