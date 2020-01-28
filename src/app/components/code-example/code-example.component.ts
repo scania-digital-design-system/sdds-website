f4e6a25c73454710c5eb84ad65f796d39b22815c
Ela Yudhanira <ela.yudhanira@scania.com>
2020-01-28 13:54:41 +0100

    Update code sample component
    - remove examples data and fetch data from content.json
    - Update code-sample component to match with data from strapi

--- src/app/components/code-example/code-example.component.ts
+++ src/app/components/code-example/code-example.component.ts
@@ -1,18 +1,11 @@
 import { Component, Input } from '@angular/core';
 
-import { default as allExamples } from '../../data/examples.json';
-
 @Component({
   selector: '[code-example]',
   templateUrl:'code-example.component.html'
 })
 
 export class CodeExampleComponent {
-  @Input() exampleId: Number;
+  @Input() example: Object;
   @Input() toggleCode: Boolean;
-  content;
-
-  ngOnInit(){
-    this.content = allExamples.find(ex => ex.id == this.exampleId) || {}
-  }
 }
