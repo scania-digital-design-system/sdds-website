ff31183076769a9788fdc8b872ea1f406288011d
Marcus Mena <marcus.mena.pacheco@scania.com>
2020-04-29 09:54:44 +0200

    Cleaning up and refactored page component
    - Removed duplicated code
    - Indentation
    - Renamed some functions and variables
    - refactored

--- src/app/components/page/page.component.ts
+++ src/app/components/page/page.component.ts
@@ -17,10 +17,10 @@ FIXME: title should fetch from the template not direct in the html, h1 for homep
 
 @Component({
   template: `
-  <h1 *ngIf="!content.title.includes('Home')">{{content.title}}</h1>
+  <h1 *ngIf="!content.title.includes('home')">{{content.title}}</h1>
   <ul *ngIf="content.contents.length > 1" class="nav" id="myTab">` +
   templates.map((page: Template) => `
-    <li *ngIf="templateCheck(${page.id})" class='nav-item'>
+    <li *ngIf="templateCheckId(${page.id})" class='nav-item'>
       <a class='nav-link' data-toggle='tab' (click)="tabChange(content, '${page.title}')" [ngClass]="dynamicActiveState(${page.id}, active)" href='#section-${page.id}'> ${page.title} </a>
     </li>
   `).join('')
@@ -39,78 +39,75 @@ FIXME: title should fetch from the template not direct in the html, h1 for homep
 
 export class PageComponent {
   content: any = {};
-  result: boolean;
   templates: any = {};
-  unique: any;
-  unique2: any;
   active: any;
 
+
   constructor(public ps: PageService) {
-    this.ps.page.subscribe((page: Page) => {
 
-      // FIXME: Improve the if statement for page.id and page.parent.id
-      if(page.id){
-        this.content = menus.find(menu => menu.id === page.id);
+    ps.page.subscribe((page: Page) => {
 
-        // For sovling the unique id and title for every template
-        const unique = [];
-        for(let [key, value] of Object.entries(this.content.contents)) {
-          if(!unique.includes(unique.includes(value['template'].title))) {
-            unique.push(value['template'].title);
-          }
-        }
-
-        const unique2 = [];
-        for(let [key, value] of Object.entries(this.content.contents)) {
-          if(!unique2.includes(unique2.includes(value['template'].id))) {
-            unique2.push(value['template'].id);
-          }
-        }
-        // The id needs to be sorted since the database is unsorted right now
-        unique2.sort()
-        // Set active for the first available template
-        this.active = unique2[0];
-
-      } else if(page.parent)  {
-        this.content = menus.find(menu => menu.id === page.parent.id);
-        // Works the same way as previous if statement
-        const unique = [];
-        for(let [key, value] of Object.entries(this.content.contents)) {
-          if(!unique.includes(unique.includes(value['template'].title))) {
-            unique.push(value['template'].title);
-          }
-        }
-        const unique2 = [];
-        for(let [key, value] of Object.entries(this.content.contents)) {
-          if(!unique2.includes(unique2.includes(value['template'].id))) {
-            unique2.push(value['template'].id);
-          }
-        }
-
-        unique2.sort();
-        this.active = unique2[0]
-
-      } else {
+      if(Object.keys(page).length == 0){
         return;
+      } else {
+        if(page.hasOwnProperty('id')){
+          this.content = menus.find(menu => menu.id === page.id);
+          // For sovling the unique id and title for every template
+          this.objFilter();
+        } else if(page.hasOwnProperty('parent') && page.parent)  {
+          this.content = menus.find(menu => menu.id === page.parent.id);
+          // Works the same way as previous if statement
+          this.objFilter();
+
+        } else {
+          return;
+        }
       }
     });
   }
 
-  ngOnInit() {
-    // TODO: Would be nice if we could use this instead. But then we
-    // need to figure out a way to let us know when this $ is available
+  objFilter() {
+    const templateTitleArr = [];
+    const templateIdArr = [];
 
-    // let $ = window.CorporateUi.$;
-    // $(() => {
-    //   $('a[data-toggle="tab"]').on('shown.bs.tab', (e) => {
-    //     console.log(
-    //       e.target, // newly activated tab
-    //       e.relatedTarget // previous active tab
-    //     );
-    //     this.tabChange();
-    //   });
-    // });
+    for(let [key, contentItem] of Object.entries(this.content.contents)) {
+      if(!templateTitleArr.includes(templateTitleArr.includes(contentItem['template'].title))) {
+        templateTitleArr.push(contentItem['template'].title);
+        templateIdArr.push(contentItem['template'].id);
+      }
+    }
+    this.sortContent(templateIdArr);
   }
+
+  // Checking to find unique id for template
+  templateCheckId(pageId) {
+    let templateid = this.content.contents.find(item => item.template.id == pageId)
+    return templateid;
+  }
+
+  sortContent(arr) {
+    //Sort the template ids for the picked content
+    arr.sort();
+    // Set active for the first available template
+    this.active = arr[0];
+  }
+
+/*  ngOnInit() {
+   // TODO: Would be nice if we could use this instead. But then we
+   // need to figure out a way to let us know when this $ is available
+
+    let $ = window.CorporateUi.$;
+    $(() => {
+      $('a[data-toggle="tab"]').on('shown.bs.tab', (e) => {
+        console.log(
+          e.target, // newly activated tab
+          e.relatedTarget // previous active tab
+        );
+        this.tabChange();
+      });
+    });
+  } */
+
 
   // Set active class on element
   dynamicActiveState(page, activePage) {
@@ -123,12 +120,6 @@ export class PageComponent {
         active: false
       }
     }
-  }
-
-  // Checking to find unique id for template
-  templateCheck(pageid) {
-    let templateid = this.content.contents.find(item => item.template.id == pageid)
-    return templateid;
   }
 
   tabChange(page, tabTitle) {
