09b576122b4a45092daf7e89b1136e45e4765362
Marcus Mena <marcus.mena.pacheco@scania.com>
2020-09-10 16:38:44 +0200

    Fix bug for table showing in every example on navigation

--- src/app/components/code-props/code-props.component.ts
+++ src/app/components/code-props/code-props.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, ElementRef } from '@angular/core';
 import { PageService } from 'src/app/app.service';
 
 @Component({
@@ -8,46 +8,42 @@ import { PageService } from 'src/app/app.service';
 })
 
 export class CodePropsComponent {
+  @Output() change: EventEmitter<Boolean> = new EventEmitter<Boolean>(); //Event to detect change in component object
   content: any; //Json data from stencil
-  component: object;
+  component: object;  component2: object;
   props: object; //Properties of every component
   tagName: string; //Tag name of the component
   empty: boolean; // If there are no data for the component
-  @Output() change: EventEmitter<Boolean> = new EventEmitter<Boolean>(); //Event to detect change in component object
+  currentCodeSample: object;
 
-  constructor(ps: PageService) {
+  constructor(ps: PageService, elref: ElementRef) {
     // Getting the documentation for every component
     this.content = ps.docs.source;
+    this.currentCodeSample = elref.nativeElement.parentElement.children;
     this.getComponent();
   }
-
+  
   getComponent() {
     //Retriving the component data from Corporate-Ui
-
     /* TODO: Look into a better way of compare if the web-component exist,
-    maybe add labels to what is a webcomponent with props */
+    maybe add labels */
     setTimeout(() =>  {
-    var el = document.querySelectorAll('c-code-sample');
-
-    el.forEach(el => {
-      this.component = this.content._value.find(component => {
-        if(el.children[0].tagName.toLowerCase() == component.tag) {
-          return component;
-        }
+      this.component = this.content._value.find((component) => {
+        let currentTagName = this.currentCodeSample[0].children[1].children[0].children[0].children[0].tagName.toLowerCase();
+        if(currentTagName == component.tag) return component
       })
-    })
 
-    if(!this.component) {
-      this.empty = true;
-      this.change.emit(this.empty)
-    }
-    else {
-      this.empty = false;
-      this.change.emit(this.empty)
-      this.getTagName(this.component);
-      this.getProps(this.component);
-    }
-  })
+      if(!this.component) {
+        this.empty = true;
+        this.change.emit(this.empty)
+      }
+      else {
+        this.empty = false;
+        this.change.emit(this.empty)
+        this.getTagName(this.component);
+        this.getProps(this.component);
+      }
+    })
   }
 
   getTagName(component) {
