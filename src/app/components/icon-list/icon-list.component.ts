419f415a957c5ee97e73d0e4f8766194f16b6c9a
Ela Yudhanira <ela.yudhanira@scania.com>
2020-10-01 11:33:20 +0200

    Add select unselect function
    - When select all, check all boxes
    - Add function to save selected items

--- src/app/components/icon-list/icon-list.component.ts
+++ src/app/components/icon-list/icon-list.component.ts
@@ -1,7 +1,5 @@
-import { Component, NgZone, Input } from '@angular/core';
-import { saveAs } from 'file-saver'
-
-import { PageService } from '../../app.service';
+import { Component, Input, OnInit } from '@angular/core';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: '[icon-list]',
@@ -9,17 +7,40 @@ import { PageService } from '../../app.service';
   styleUrls: ['./icon-list.component.scss']
 })
 
-export class IconListComponent {
+export class IconListComponent implements OnInit {
   @Input() icons: Array<Object>;
   @Input() lastUpdate: Date;
+  
+  // current icon opened in modal
   currentIcon: Object = {};
 
-  constructor(private ps: PageService, private zone: NgZone) { }
+  // a value that represents Select All status
+  // bind to (ngModel), value will change automatically when checkbox selected / deselected
+  isSelectedAll: Boolean = false;
+
+  // checked icons
+  checkedIcons: Array<String> = [];
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    // assign initial state in icons object
+    this.icons.map((item) => {
+      return item['isSelected'] = false;
+    })
+  }
 
   openModal(icon) {
     this.currentIcon = icon;
     // Because of innerHTML, need to have the code-sample here, cannot be rendered from html
     this.currentIcon['code'] = `<c-code-sample><c-icon name="${icon.name}"></c-icon></c-code-sample>`;
+  }
+
+  getModalLayout(currentIcon){
+    // If description, usage, and restriction is not provided
+    // Then modal size = default, icon column = col-md-12
+    return currentIcon.description || currentIcon.usage || currentIcon.restriction;
   }
 
   download(event) {
@@ -32,11 +53,38 @@ export class IconListComponent {
     saveAs(url, fileName + '.svg');
   }
 
-  getModalLayout(currentIcon){
-    // If description, usage, and restriction is not provided
-    // Then modal size = default, icon column = col-md-12
-    return currentIcon.description || currentIcon.usage || currentIcon.restriction;
+  clickIconCheckbox(event) {
+    // on checkbox clicked, avoid interfere with action click to open modal
+    event.stopPropagation();
   }
 
+  // function to select / deselect all other checkboxes in each icons
+  selectAll() {
+    this.icons.forEach((item) => {
+      item['isSelected'] = this.isSelectedAll;
+    })
+    
+    // get checked items
+    this.getCheckedIcons();
+  }
+
+  selectIconCheckbox() {
+    // on isSelectedAll = true, if unselect one checkbox, change isSelectedAll to false
+    this.isSelectedAll = this.icons.every(function(item) {
+      return item['isSelected'] == true;
+    })
+
+    // get checked items
+    this.getCheckedIcons();
+  }
+
+  getCheckedIcons() {
+    this.checkedIcons = [];
+    this.icons.forEach((icon:any) => {
+      if(icon.isSelected) {
+        this.checkedIcons = [...this.checkedIcons, icon.name]
+      }
+    })
+  }
   
 }
