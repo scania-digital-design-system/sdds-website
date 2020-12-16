e2d05048445b72b1dbf5d554000c66093f6a15a8
Ela Yudhanira <ela.yudhanira@scania.com>
2020-12-16 11:01:14 +0100

    Fix scrolling problem

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -45,9 +45,13 @@ export class MainComponent{
         this.ps.setPage(this.page);
 
         this.analytics(route.url);
-
+        
         const contentContainer = document.querySelector('main') || window;
-        contentContainer.scrollTo(0, 0);
+        
+        if(paths.length == 0 || (paths.length > 0 && paths[0].indexOf('#') < 0)) {
+          contentContainer.scrollTo(0, 0);
+        }
+        
       }
     });
   }
